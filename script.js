969b45e5141a30e3dac50dc52342311a1e91102a
Adnan Aheer <adnanaheer20@gmail.com>
2021-07-08 00:20:29 +0500

    data showing with dom

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
 let apiQuotes = []
 
 // show new quote
 function newQuote() {
     // pick a random quote from apiQuote array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    // check if author field is blank and replace it with 'unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown'
+    } else {
+        authorText.textContent = quote.author
+    }
+
+    // check quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote')
+    } else {
+        quoteText.classList.add('long-quote')
+    }
+    quoteText.textContent = quote.text
+
 }
 
 
@@ -18,6 +39,18 @@ async function getQuotes() {
         // catch error here
     }
 }
+
+// tweet quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.
+        textContent} - ${authorText.textContent}`
+        window.open( twitterUrl, '_blank')
+}
+
+//  event listner
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
+
 
 //  on loaded
 getQuotes()
